03abcdbda6b25bce9c608cf5c5b93c08c208cc42
Dhruv Chawla <44582521+dc03@users.noreply.github.com>
2023-02-12 22:36:44 +0530

    feat: update Config.h

--- include/Config.h
+++ include/Config.h
@@ -22,21 +22,29 @@ constexpr static uint8_t ROWS[] = {5, 6, 7};
 constexpr static uint8_t GREEN_COLS[] = {13, 11, 9};
 constexpr static uint8_t RED_COLS[] = {12, 10, 8};
 constexpr static uint8_t PINS[] = {5, 6, 7, 8, 9, 10, 11, 12, 13};
-constexpr static uint8_t BUTTONS[] = {A0, A1, A2, A3, A4, A5};
+
+constexpr static uint8_t BUTTONS_COLS[] = {A0, A1, A2};
+constexpr static uint8_t BUTTONS_ROWS[] = {A3, A4, A5};
+
+#define SLIDE_LEFT     4
+#define SLIDE_RIGHT    3
+#define SLIDE_IS_LEFT  0
+#define SLIDE_IS_RIGHT 1
 
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
 
 static_assert(ARRAY_SIZE(GREEN_COLS) == ARRAY_SIZE(RED_COLS), "Expect green and red to be of same size");
 
-#define ROW_COUNT    ARRAY_SIZE(ROWS)
-#define COL_COUNT    ARRAY_SIZE(GREEN_COLS)
-#define PIN_COUNT    ARRAY_SIZE(PINS)
-#define BUTTON_COUNT ARRAY_SIZE(BUTTONS)
+#define ROW_COUNT        ARRAY_SIZE(ROWS)
+#define COL_COUNT        ARRAY_SIZE(GREEN_COLS)
+#define PIN_COUNT        ARRAY_SIZE(PINS)
+#define BUTTON_ROW_COUNT ARRAY_SIZE(BUTTONS_ROWS)
+#define BUTTON_COL_COUNT ARRAY_SIZE(BUTTONS_COLS)
 
 #define DELAY          1
 #define DELAY_FUNCTION delay
-#define TIMES_TO_RUN   20
-#define PAUSE_DELAY    75
+#define TIMES_TO_RUN   50
+#define PAUSE_DELAY    200
 
 static_assert(TIMES_TO_RUN < static_cast<uint8_t>(-1), "TIMES_TO_RUN should not exceed maximum uint8_t value");
 
