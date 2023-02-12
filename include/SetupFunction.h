d4be735216fa604fda0dd65ca2681b74f9d82adc
Dhruv Chawla <44582521+dc03@users.noreply.github.com>
2023-02-12 22:37:49 +0530

    refactor: update tictactoe_setup()

--- include/SetupFunction.h
+++ include/SetupFunction.h
@@ -4,6 +4,12 @@
 #define SETUP_FUNCTION_H
 
 #include <Config.h>
+#include <GameState.h>
+#include <SlideSwitch.h>
+
+void turnOffAllLEDs();
+
+GameState state = makeState(0, 0);
 
 static inline void tictactoe_setup() {
 #if USE_SERIAL
@@ -12,9 +18,16 @@ static inline void tictactoe_setup() {
   for (uint8_t i = 0; i < PIN_COUNT; i++) {
     pinMode(PINS[i], OUTPUT);
   }
-  for (uint8_t i = 0; i < BUTTON_COUNT; i++) {
-    pinMode(BUTTONS[i], INPUT_PULLUP);
+  for (uint8_t i = 0; i < BUTTON_COL_COUNT; i++) {
+    pinMode(BUTTONS_COLS[i], OUTPUT);
+    digitalWrite(BUTTONS_COLS[i], LOW);
   }
+  for (uint8_t i = 0; i < BUTTON_ROW_COUNT; i++) {
+    pinMode(BUTTONS_ROWS[i], INPUT_PULLUP);
+  }
+  pinMode(SLIDE_LEFT, INPUT_PULLUP);
+  pinMode(SLIDE_RIGHT, INPUT_PULLUP);
+  turnOffAllLEDs();
 }
 
 #endif
