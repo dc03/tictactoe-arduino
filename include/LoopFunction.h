41e837798860051bfbac5964e5e991e8647a1654
Dhruv Chawla <44582521+dc03@users.noreply.github.com>
2023-03-05 09:43:21 +0530

    feat: add caching of previous press and AI call to LoopFunction

--- include/LoopFunction.h
+++ include/LoopFunction.h
@@ -37,13 +37,18 @@ static inline void detectWin() {
 }
 
 static inline uint8_t handlePlayerInput() {
+  static uint8_t prev_button = 0;
   uint8_t button_pressed = pressedButton();
-  if (button_pressed) {
+  if (button_pressed && button_pressed != prev_button) {
     uint8_t row = buttonRow(button_pressed);
     uint8_t col = buttonCol(button_pressed);
+    if (!isCellEmpty(state, row, col)) { return 0; }
     state = trySetCell(state, row, col);
+    prev_button = button_pressed;
+    return button_pressed;
+  } else {
+    return 0;
   }
-  return button_pressed;
 }
 
 static inline void tictactoe_1P_mode() {
@@ -51,7 +56,12 @@ static inline void tictactoe_1P_mode() {
   uint8_t button_pressed = 0;
   if ((button_pressed = handlePlayerInput())) {
     detectWin();
-    makeAIMove(state, buttonRow(button_pressed), buttonCol(button_pressed));
+    for (uint16_t i = 0; i < 50; i++) {
+      drawGameStateEquiDelay(state);
+      DELAY_FUNCTION(DELAY);
+    }
+    state = makeAIMove(state);
+    detectWin();
   }
 }
 
